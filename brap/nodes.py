a7f4dc9370d70446123072bad8040b5b2bd8fb54
Brian Graham <brian@statagroup.com>
2017-07-09 15:01:53 -0400

    Nodes with tests

--- brap/nodes.py
+++ brap/nodes.py
@@ -17,25 +17,22 @@ class RegisteredNode(Node):
 
    # FIXME edges can be put into registration object instead of graph or injected here.
     def __init__(self, registration=None):
-
-        super().__init__(node_id, edges)
-        self._value = value
         self._registration = registration  # TODO ensure is instance of registration
         self._tags = []
 
     def get_id(self):
-        self._registration.get_id()
+        return self._registration.get_id()
 
     def get_value(self):
         raise Exception(
             'Registered node "{}" was requested without being compiled.'.format(self._id))
 
     def get_edges(self):
-        self._registration.get_edges()
+        return self._registration.get_edges()
 
     def set_tags(self, tags):
         self._tags = tags
 
-    def get_tags(self, tags):
+    def get_tags(self):
         return self._tags
 
