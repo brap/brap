b9eb0233b8d617dc5909b69bf23346b810d4b3f5
Brian Graham <brian@statagroup.com>
2017-07-23 11:43:54 -0400

    Tests pass

--- brap/compilers/eager_dependency_injection_compiler.py
+++ brap/compilers/eager_dependency_injection_compiler.py
@@ -4,16 +4,52 @@ from brap.compilers.compiler import Compiler
 from brap.nodes import Node
 
 
-class EagerDependencyInjectionCompiler(object):
-    def __init__(self, service_node, function_node, parameter_node):
-        pass
+class EagerNode(Node):
+    def __init__(self):
+        self._id = None
 
-    def compile(self, graph):
-        nodes = graph.get_nodes()
+    def get_id(self):
+        return self._id
 
-        for node_id in nodes:
-            eager_node = self.transform_registered_node(nodes[node_id])
-            pass
+    def get_value(self):
+        raise Exception('Abstract Eager Nodes do not have values')
+
+
+class ServiceEagerNode(EagerNode):
+    def __init__(self, id, instance):
+        self._id = id
+        self._instance = instance
+
+    def get_value(self):
+        return self._instance
+
+class ParameterEagerNode(EagerNode):
+    def __init__(self, id, value):
+        self._id = id
+        self._value = value
+
+    def get_value(self):
+        return self._value
+
+
+class FunctionEagerNode(EagerNode):
+    def __init__(self, id, func):
+        self._id = id
+        self._func = func
+
+    def get_value(self):
+        return self._func
+
+
+# TODO more meta-level, not sure how to define this one. yet.
+class FactoryEagerNode(EagerNode):
+    def __init__(self):
+        self._id = None
+        self._instance = None
+
+    def get_value(self):
+        raise Exception('TODO')
+
 
 class EagerNodeFactory():
     def from_registration():
@@ -66,48 +102,14 @@ class ParameterEagerNodeFactory(EagerNode):
         value = 'fixme value'
         return ServiceEagerNode(id, value)
 
-class EagerNode(Node):
+
+class EagerDependencyInjectionCompiler(object):
     def __init__(self):
-        self._id = None
+        pass
 
-    def get_id(self):
-        return self._id
+    def compile(self, graph):
+        nodes = graph.get_nodes()
 
-    def get_value(self):
-        raise Exception('Abstract Eager Nodes do not have values')
-
-
-class ServiceEagerNode(EagerNode):
-    def __init__(self, id, instance):
-        self._id = id
-        self._instance = instance
-
-    def get_value(self):
-        return self._instance
-
-class ParameterEagerNode(EagerNode):
-    def __init__(self, id, value):
-        self._id = id
-        self._value = value
-
-    def get_value(self):
-        return self._value
-
-
-class FunctionEagerNode(EagerNode):
-    def __init__(self, id, func):
-        self._id = id
-        self._func = func
-
-    def get_value(self):
-        return self._func
-
-
-# TODO more meta-level, not sure how to define this one. yet.
-class FactoryEagerNode(EagerNode):
-    def __init__(self):
-        self._id = None
-        self._instance = None
-
-    def get_value(self):
-        raise Exception('TODO')
+        for node_id in nodes:
+            # eager_node = self.transform_registered_node(nodes[node_id])
+            pass
